4a862377670d3645032b3b1d4cf1a12a668bb6ce
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 15:44:26 +0930

    Minor refactoring

--- server/api/api.js
+++ server/api/api.js
@@ -5,15 +5,13 @@ const apiUrl = "http://v0.postcodeapi.com.au/";
 const getAllSuburbsByPostcode = async (params) => {
     const { code: postcode } = params;
     const response = await fetch(`${apiUrl}suburbs.json?postcode=${postcode}`);
-    const data =  await response.json();
-    return data;
+    return await response.json();    
 }
 
 const getAllSuburbsInRange = async (params) => {
     const { latitude, longitude, distance } = params;
     const response = await fetch(`${apiUrl}radius.json?latitude=${latitude}&longitude=${longitude}&distance=${distance}`);
-    const data =  await response.json();
-    return data;
+    return await response.json();    
 }
 
 module.exports = {
