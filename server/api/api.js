475db4f501b04d00d04912fcf81c6211c3bceb3a
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 15:37:54 +0930

    Fixed URLs in fetch calls by correcting template strings

--- server/api/api.js
+++ server/api/api.js
@@ -1,18 +1,17 @@
 const fetch = require('node-fetch');
 
 const apiUrl = "http://v0.postcodeapi.com.au/";
-const suburbsRequestUrl = `${apiUrl}suburbs.json`;
 
-async function getAllSuburbsByPostcode(params) {
-    const { postcode } = params;
-    const response = await fetch(`${suburbsRequestUrl}?postcode="${postcode}"`);    
+const getAllSuburbsByPostcode = async (params) => {
+    const { code: postcode } = params;
+    const response = await fetch(`${apiUrl}suburbs.json?postcode=${postcode}`);
     const data =  await response.json();
     return data;
 }
 
-async function getAllSuburbsInRange(params) {
+const getAllSuburbsInRange = async (params) => {
     const { latitude, longitude, distance } = params;
-    const response = await fetch(`${apiUrl}radius.json?latitude="${latitude}"&longitude="${longitude}&distance="${distance}`);
+    const response = await fetch(`${apiUrl}radius.json?latitude=${latitude}&longitude=${longitude}&distance=${distance}`);
     const data =  await response.json();
     return data;
 }
@@ -21,3 +20,4 @@ module.exports = {
     getAllSuburbsByPostcode,
     getAllSuburbsInRange
 }
+
