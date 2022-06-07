521ec14d0c5d517e2cad4cf5d9c46bd7d5074046
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 22:40:49 +0930

    Added missing material imports

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,8 +19,9 @@ import { MaterialExampleModule } from './../material.module';
     SuburbItemComponent,
   ],
   imports: [
-
-  BrowserModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatNativeDateModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
