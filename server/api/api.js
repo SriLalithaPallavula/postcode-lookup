81e5637779b7b25f4fc21807c21e7f83ed9cadd2
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-08 08:32:01 +0930

    Added function documentation to API request methods in Server

--- server/api/api.js
+++ server/api/api.js
@@ -2,12 +2,22 @@ const fetch = require('node-fetch');
 
 const apiUrl = "http://v0.postcodeapi.com.au/";
 
+/**
+ * Makes an API request to postcode api to get all suburbs matching a postcode
+ * @returns a promise with an array of suburbs
+ * @param params params in client url request
+ */
 const getAllSuburbsByPostcode = async (params) => {
     const { code: postcode } = params;
     const response = await fetch(`${apiUrl}suburbs.json?postcode=${postcode}`);
     return await response.json();    
 }
 
+/**
+ * Makes an API request to postcode api to get all suburbs within a specific distance from the given suburb
+ * @returns a promise with an array of suburbs
+ * @param params params in client url request, contains props latitude, longitude and distance
+ */
 const getAllSuburbsInRange = async (params) => {
     const { latitude, longitude, distance } = params;
     const response = await fetch(`${apiUrl}radius.json?latitude=${latitude}&longitude=${longitude}&distance=${distance}`);
