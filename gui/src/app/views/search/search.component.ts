b10a26315cad9c9704ba0a512b270e2d2517b613
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 22:40:07 +0930

    Modified search component to make search requests and set results on the main app

--- gui/src/app/views/search/search.component.ts
+++ gui/src/app/views/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { SearchPostcodeService } from '../../services/search-postcode.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.less']
 })
 export class SearchComponent implements OnInit {
+  postcode: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+  ]);
+  searchForm = new FormGroup({ postcode: this.postcode });
+  isFormSubmitted: boolean = false;
 
-  constructor() { }
+  @Output() emitSuburbResults = new EventEmitter<[]>();
+
+  constructor(private searchPostcodeService: SearchPostcodeService) {}
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.isFormSubmitted = true;
+    if (this.searchForm.valid) {
+      this.searchPostcodeService
+        .getAllSuburbsByPostcode(this.searchForm.controls.postcode.value)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.emitSuburbResults.next(response);
+          }
+        );
+    }
   }
 
 }
