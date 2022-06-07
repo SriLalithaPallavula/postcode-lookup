68912b176e5f9d01054d0bb94ac9b4ffe1803660
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 22:41:35 +0930

    Modified main app component to send data and user interaction events between the components

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchPostcodeService } from '../app/services/search-postcode.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'gui';
+  title = 'postcode lookup';
+  range = 4000;
+  suburbsList = [];
+  suburbsListInRange = [];
+
+  constructor(private searchPostcodeService: SearchPostcodeService){}
+
+  setSuburbResults(data : any){
+    this.suburbsList = data;
+    this.suburbsListInRange = [];
+  }
+
+  handleOnSuburbSelect(data : any){
+    this.searchPostcodeService.getAllSuburbsInRange(data, this.range).subscribe(data => {
+      this.suburbsListInRange = data;
+    });
+  }
 }
