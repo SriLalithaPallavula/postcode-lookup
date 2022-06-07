2fe2a4d386d4aadfe1bedd9c0d8c793d4bbc9842
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 19:49:38 +0930

    imported FormModules and added material module file

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './views/search/search.component';
 import { SuburbsListComponent } from './views/suburbs-list/suburbs-list.component';
-import { SuburbItemComponent } from './views/suburb-item/suburb-item.component';
+import { SuburbItemComponent } from './views/suburb-item/suburb-item.component';;
 
 @NgModule({
   declarations: [
@@ -16,7 +17,10 @@ import { SuburbItemComponent } from './views/suburb-item/suburb-item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
