334bcf6c3cd3711a8b82d2844dd43762820b1e90
SriLalithaPallavula <sri26lalitha@gmail.com>
2022-06-07 22:37:39 +0930

    Added this component to render list of suburbs as material cards
    This component can be used to display the search results (left pane) and filtered results by specific distance (right pane). The component has paginator which shows 10 items per page by default.

--- gui/src/app/views/suburb-item/suburb-item.component.ts
+++ gui/src/app/views/suburb-item/suburb-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Suburb } from 'src/app/models/suburb.model';
 
 @Component({
   selector: 'app-suburb-item',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuburbItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() suburb!: Suburb;
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  ngOnInit() {
   }
 
 }
--- gui/src/app/views/suburbs-list/suburbs-list.component.ts
+++ gui/src/app/views/suburbs-list/suburbs-list.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, SimpleChanges, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { SearchPostcodeService } from '../../services/search-postcode.service';
+import { Suburb } from '../../models/suburb.model';
 
 @Component({
   selector: 'app-suburbs-list',
   templateUrl: './suburbs-list.component.html',
   styleUrls: ['./suburbs-list.component.less']
 })
-export class SuburbsListComponent implements OnInit {
+export class SuburbsListComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() suburbs: Array<Suburb> = [];
+  @Input() viewType!: string;
+  @Output() emitOnSelectSuburb = new EventEmitter();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
+  suburbsPaginated: Array<Suburb> = [];
+  selectedSuburb : Suburb;
+  length = 0;
+  pageSize = 10;
+  paginatorSubscriber!: Subscription;
+
+  ngOnInit():void{
+  }
+
+  constructor(public searchPostcodeService : SearchPostcodeService) {
+      this.selectedSuburb = this.searchPostcodeService.getSelectedSuburb();
+  }
+
+  ngAfterViewInit(){
+    if(this.paginator) this.paginator.firstPage();
+    this.subscribePaginator();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    if(changes['suburbs']){
+      this.length = this.suburbs.length;
+      this.suburbsPaginated = this.suburbs.slice(0, this.pageSize);
+      if(this.paginator) this.paginator.firstPage();
+      this.subscribePaginator();
+    }
+  }
+
+
+  onSuburbSelect(suburb: Suburb){
+    if(this.viewType === "SEARCH_RESULTS") {
+     this.emitOnSelectSuburb.next(suburb);
+     this.selectedSuburb = suburb;
+    }
+  }
+
+  isLinkActive(suburb : Suburb){
+    if(this.selectedSuburb){
+      return suburb.name === this.selectedSuburb.name;
+    }
+    return false;
+  }
+
+  subscribePaginator(){
+    if(!this.paginator) return;
+    if(this.paginatorSubscriber) this.paginatorSubscriber.unsubscribe();
+    this.paginatorSubscriber = this.paginator.page.subscribe((eventObj) => {
+      const { pageIndex, pageSize } = eventObj;
+      this.suburbsPaginated = this.suburbs.slice(pageIndex * pageSize, pageIndex * pageSize + pageSize);
+      this.length = this.suburbs.length;
+    });
   }
 
 }
